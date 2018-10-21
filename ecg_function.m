58f8d5ecfb71aadfda216f238aeb70d4c4d71b7f
Madalina BOBOC <madaeboboc@gmail.com>
2018-10-21 21:04:47 +0300

    2nd commit

--- ecg_function.m
+++ ecg_function.m
@@ -1,5 +1,5 @@
 
-function id = ecg_function(input_signal)
+function id = ecg_function(input_signal, israw)
     id = 0;
 
     Fs = 500;           %Sampling frequency
@@ -7,13 +7,25 @@ function id = ecg_function(input_signal)
     L = 5000;           %Length of signal
     t = (0:L-1)*T;      %Time vector
     f = Fs*(0:(L/2))/L;
+    if(israw == 1)
+ %      fc = 40;
+ %      Wn = (2/Fs)*fc;
+ %      b = fir1(20,Wn,'low',kaiser(21,140));
+%         windowSize = 5; 
+%         b = (1/windowSize)*ones(1,windowSize);
+%         a = 1;
+%         input_singal = filter(b,a,input_signal);
+%         w = fir1(34,0.48,'high',chebwin(35,30));
+%         input_signal = conv(input_signal, w);
+    end
     Y = fft(input_signal);
 
     P2 = abs(Y/L);
     P1 = P2(1:L/2+1);
     P1(2:end-1) = 2*P1(2:end-1);
-    pks_sample = maxk(P1, 6);
+    pks_sample = maxk(P1, 20);
     %norm_sample = norm(pks_sample)
+    
     for i = 1:90
        c =int2str(i);
        if i < 10
@@ -31,7 +43,7 @@ function id = ecg_function(input_signal)
         P1_database = P2_database(1:L/2+1);
         P1_database(2:end-1) = 2*P1_database(2:end-1);
        
-       pks_database = maxk(P1_database, 6); 
+       pks_database = maxk(P1_database, 20); 
        for j = 1 : 6
         dif(j) = pks_database(j) - pks_sample(j);
        end
@@ -41,5 +53,6 @@ function id = ecg_function(input_signal)
     index = 1:90;
     matrix = [values(:), index(:)];
     matrix = sortrows(matrix);
-    disp(matrix(1,2));
+    id = matrix(1,2);
+    %disp(matrix(1,2));
 end
--- ex2.m
+++ ex2.m
@@ -1,11 +1,23 @@
-
-fclose all
-clear all
-close all
-clc
-
-fileID = fopen("ECG-DB\ECG-DB\Person_03\rec_1m.mat");
-var1 = importdata("ECG-DB\ECG-DB\Person_03\rec_1m.mat");
-var = var1(2,:);
-fclose(fileID);
-ecg_function(var);
+%%%%%%%%%%              checker                 %%%%%%%%%%%
+israw = 1;
+count = 0;
+for i = 1:90
+   c =int2str(i);
+   if i < 10
+        str = strcat('ECG-DB\ECG-DB\Person_0', c, '\', 'rec_1m.mat');
+   else
+        str = strcat('ECG-DB\ECG-DB\Person_', c, '\', 'rec_1m.mat');
+   end
+   fid = fopen(str);
+   data_sample = importdata(str);
+   fclose(fid);
+   if(israw == 0)
+       sample_signal = data_sample(2, :);
+   else
+       sample_signal = data_sample(1, :);
+   end
+   if(ecg_function(sample_signal, israw) == i)
+       count = count + 1
+   end
+end
+disp(count);
